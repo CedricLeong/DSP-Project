5e0c56ebeba10d7dedd3a937dbabaac89ecd01eb
Zilenc3 <Dwayne.alex@uoit.net>
2014-11-11 11:56:04 -0500

    Changed Number Detection.
    My NumberDection was too complex after I thought about how Simulink was
    going to detect it. Instead NumberDetection will now send the signal
    through 10 different filters.
    
    Then sum the signal

--- Decoder/NumberDetection.m
+++ Decoder/NumberDetection.m
@@ -4,37 +4,34 @@
 %the number of samples is greater than X, with an amplitude of >=1, then
 %this will be detected as a number. 
 
-%The function will then discard samples 20% of the samples from each end. 
-%Doing this we will have a signal of maximum length of 33ms and minimum
-%length of 24ms. Cutting the signal view from each end will help us distinguish 
-%what is the signal by discarding any noise before or after.
 
-%Note this is NOT a filter, just a precursor to being filtered.
+function  NumberDetection() 
+	curDir   = pwd; %This is just pathing. Ignore the next three lines.
+	mainDir  = fileparts(fileparts(curDir));
+	newPath = fullfile(mainDir, 'DSP-Project', 'Encoder');
+	addpath (newPath);
 
-function [A] = NumberDetection() 
 
 	Signal = encoderdsp (); %Fetch the signal from the encoder
 	len = length(Signal);	%Get length of signal 
 	Fs = 8000; %Sampling frequency
 
-	for (l:len) 
+	Wp75 = [60,90]/4000; Ws75 = [50,100]/4000;
+	Rp75 = 2; Rs75 = 60;
+	[n75,Wp75] = ellipord (Wp75,Ws75,Rp75,Rs75);
+    [b,a] = ellip(n75,Rp75,Rs75,Wp75);          % Elliptic filter design
+    freqz(b,a,512,8000)                 % Plots the frequency response
 
-		if (abs(Signal(1,l)) >=1) 
-			j=0; %Counter for number of samples with abs >=1
 
-			for k=l:len %check the rest of the array for a signal with
-				j++;		  %abs >=1 and capture in an array. 
-				B (1,j) = Signal(1,k);
-
-				if (abs(Signal(k))<1) %If the signal is small, discard results
-
-					break;
-				end
-
-			end
-
-		end
-	end
 end		
 
-
+  % % Example 2:
+  %   %   Design a bandpass filter with a passband of 60 Hz to 200 Hz, with 
+  %   %   less than 3 dB of ripple in the passband, and 40 dB attenuation in 
+  %   %   the stopbands that are 50 Hz wide on both sides of the passband.
+ 
+  %   Wp = [60 200]/500; Ws = [50 250]/500;
+  %   Rp = 3; Rs = 40;
+  %   [n,Wp] = ellipord(Wp,Ws,Rp,Rs)      % Gives mimimum order of filter      
+  %   [b,a] = ellip(n,Rp,Rs,Wp);          % Elliptic filter design
+  %   freqz(b,a,512,1000)                 % Plots the frequency response
