c1964a6ae575a39016d284aa885286bacd98ae65
Zilenc3 <dwayne.alex@uoit.net>
2014-11-12 17:20:45 -0500

    Changed Filter Design & Made a Filter
    filterr is a 75Hz filter made in FDATOOL
    
    RectifyingFilter
    - The only working filter is the 100Hz filter, which pickups both 75Hz
    and 100Hz.
    - These filters when limited to a small BP region do not work for some
    reason.

--- Decoder/RectifyingFilter.m
+++ Decoder/RectifyingFilter.m
@@ -15,19 +15,33 @@ function [Signal] = RectifyingFilter()
 	addpath (newPath);
 
 	close all;
-	Signal = fixed_encoder (); %Fetch the signal from the encoder
+	[Signal,No] = fixed_encoder (); %Fetch the signal from the encoder
 	len = length(Signal);	%Get length of signal 
-	Fs = 8000; %Sampling frequency
+	
 
-	Wp75 = [60,90]/(0.5*Fs); Ws75 = [25,200]/(Fs*0.5);
-	Rp75 = 2; Rs75 = 60;
-	[n75,Wp75] = ellipord (Wp75,Ws75,Rp75,Rs75);
-    [b75,a75] = ellip(n75,Rp75,Rs75,Wp75);          % Elliptic filter design
-    freqz(b75,a75,512,Fs);                 % Plots the frequency response
-    title('WP75');
+    Fs = 10000; %Sampling frequency
+    T = 1/Fs; % Sample time
+    L = length(Signal);
+    t = (0:L-1)*T;% Time vector
+
+    NFFT = 2^nextpow2(L);
+    Y = fft(Signal,NFFT)/L;
+    f = Fs/2*linspace(0,1,NFFT/2+1);
+    % plot(f,2*abs(Y(1:NFFT/2+1))) 
+
+  
+
+    % plot(abs(fft(Signal)));
+
+    Sig1 = filterr(Signal);
     figure;
-    Sig1 = filter(b75,a75,fft(Signal));
-    plot(abs(Sig1));
+    L = length(Sig1);
+    Y = fft(Sig1,NFFT)/L;
+    f = Fs/2*linspace(0,1,NFFT/2+1);
+    plot(f,2*abs(Y(1:NFFT/2+1)));
+    title('75Hz');
+    axis([0 500 0 1]);
+    figure
 
 	Wp100 = [60,140]/(0.5*Fs); Ws100 = [30,260]/(0.5*Fs);
 	Rp100 = 2; Rs100 = 60;
@@ -36,20 +50,25 @@ function [Signal] = RectifyingFilter()
     % freqz(b100,a100,512,Fs);
     % title('WP100');
     % figure;
-    figure;
-    Sig2 = filter(b100,a100,fft(Signal));
-    plot(abs(Sig2));
+    Sig2 = filter(b100,a100,Signal);
+    Y = fft(Sig2,NFFT)/L;
+    plot(f,2*abs(Y(1:NFFT/2+1))) 
+    title('100Hz');
+    axis([0 500 0 1]);
+
 
  %    Wp150 = [130,170]/(0.5*Fs); Ws150 = [100 190]/(0.5*Fs);
-	% Rp150 = 2; Rs150 = 60;
-	% [n150,Wp150] = ellipord (Wp150,Ws150,Rp150,Rs150);
+ %    Rp150 = 2; Rs150 = 60;
+ %    [n150,Wp150] = ellipord (Wp150,Ws150,Rp150,Rs150);
  %    [b150,a150] = ellip(n150,Rp150,Rs150,Wp150);
  %    % freqz(b150,a150,512,Fs);   
  %    % title('WP150');
- %    % figure;
  %    figure;
  %    Sig3 = fft(filter(b150,a150,Signal));
- %    plot(abs(Sig3));
+ %    Y = fft(Sig3,NFFT)/L;
+ %    plot(f,2*abs(Y(1:NFFT/2+1)))
+ %    title('150Hz');
+ %    axis([0 500 0 1]);
 
 
  %    Wp200 = [180,220]/(0.5*Fs); Ws200 = [140,260]/(0.5*Fs);
@@ -57,11 +76,12 @@ function [Signal] = RectifyingFilter()
 	% [n200,Wp200] = ellipord (Wp200,Ws200,Rp200,Rs200);
  %    [b200,a200] = ellip(n200,Rp200,Rs200,Wp200);
  %    % freqz(b200,a200,512,Fs);   
- %    % title('WP200');   
- %    % figure;
  %    figure;
  %    Sig4 = fft(filter(b200,a200,Signal));
- %    plot(abs(Sig4));
+ %    Y = fft(Sig4,NFFT)/L;
+ %    plot(f,2*abs(Y(1:NFFT/2+1))) 
+ %    title('200Hz');   
+ %    axis([0 500 0 1]);
 
 
  %    Wp300 = [280,320]/(0.5*Fs); Ws300 = [260,340]/(0.5*Fs);
@@ -69,27 +89,36 @@ function [Signal] = RectifyingFilter()
 	% [n300,Wp300] = ellipord (Wp300,Ws300,Rp300,Rs300);
  %    [b300,a300] = ellip(n300,Rp300,Rs300,Wp300); 
  %    % freqz(b300,a300,512,Fs);   
- %    % title('WP300'); 
- %    % figure;  
+ %    figure;  
+ %    Sig5 = fft(filter(b300,a300,Signal));
+ %    Y = fft(Sig5,NFFT)/L;
+ %    plot(f,2*abs(Y(1:NFFT/2+1)));
+ %    title('300Hz'); 
+ %    axis([0 500 0 1]);
 
  %    Wp400 = [380,420]/(0.5*Fs); Ws400 = [360,440]/(0.5*Fs);
 	% Rp400 = 2; Rs400 = 60;
 	% [n400,Wp400] = ellipord (Wp400,Ws400,Rp400,Rs400);
  %    [b400,a400] = ellip(n400,Rp400,Rs400,Wp400);  
  %    % freqz(b400,a400,512,Fs);   
- %    % title('WP400')
- %    % figure; 
- %    figure;
- %    Sig1 = fft(filter(b400,a400,Signal));
- %    plot(abs(Sig1));
-
-   
+ %    figure;  
+ %    Sig5 = fft(filter(b300,a300,Signal));
+ %    Y = fft(Sig5,NFFT)/L;
+ %    plot(f,2*abs(Y(1:NFFT/2+1)));
+ %    title('400Hz'); 
+ %    axis([0 500 0 1]);
+  
  %    Wp500 = [480,520]/(0.5*Fs); Ws500 = [460,540]/(0.5*Fs);
 	% Rp500 = 2; Rs500 = 60;
 	% [n500,Wp500] = ellipord (Wp500,Ws500,Rp500,Rs500);
  %    [b500,a500] = ellip(n500,Rp500,Rs500,Wp500);   
  %    % freqz(b500,a500,512,Fs);   
- %    % title('WP500');
+ %    figure;  
+ %    Sig5 = fft(filter(b300,a300,Signal));
+ %    Y = fft(Sig5,NFFT)/L;
+ %    plot(f,2*abs(Y(1:NFFT/2+1)));
+ %    title('500Hz');
+ %    axis([0 500 0 1]);
 
 end		
 
