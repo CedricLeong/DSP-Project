db1b32c2e7623adbbbfe77a0c4b8c6b06bc56ba4
Zilenc3 <dwayne.alex@uoit.net>
2014-11-24 21:20:34 -0500

    Commenting
    Added comments to these files

--- Final Project/MATLAB/DigitOutput.m
+++ Final Project/MATLAB/DigitOutput.m
@@ -1,8 +1,17 @@
 function [LogicMatrix] = DigitOutput
+
+%This function takes our Goertzel outputs the digit accordingly.
+
+
 close all
 clear all 
 clc
 [a] = GoertzelFilter;
+digitMatrix = [0 1 2 3 4 5 6 7 8 9 ; 84 44 42 41 52 50 49 76 74 73];
+digitMatrix = digitMatrix(:);
+digitMatrix=transpose(digitMatrix);
+Output=[];
+
 if [a]== 0
     fprintf('An error occured, no tones detected')
 end
@@ -12,6 +21,9 @@ col = sizesA(1,2);
 f = [75 100 150 200 300 400 500];
 f1=[0:500];
 binaryN= [];
+
+%This for loop pulls the highest three frequencies from the Goertzel array after
+%re-ordering the array
 for i=1:col
     array1=a((1:7),i);
     a1=sort(array1,'descend');
@@ -19,15 +31,16 @@ for i=1:col
     binaryN = [binaryN bin1];
 end
 binaryNTr= transpose(binaryN);
+
+%The new array is then converted from binary to decimal
 BTd=bi2de(binaryNTr,'left-msb');
+LenD=length(BTd); %number of digits
 
 
-digitMatrix = [0 1 2 3 4 5 6 7 8 9 ; 84 44 42 41 52 50 49 76 74 73];
-digitMatrix = digitMatrix(:);
-digitMatrix=transpose(digitMatrix);
-LenD=length(BTd);
-Output=[];
-for i =1:LenD
+
+%this for loop compares the decimal equivalent to the corresponding number
+%it then uses a circular shift to arrange the matrix for the final output.
+for i=1:LenD
     a=digitMatrix == BTd(i);
     a=circshift(a,[-1,-1]);
     Output=[Output digitMatrix(a)];    
@@ -36,13 +49,7 @@ if isempty(Output)
 	fprintf('No tones Detected.')
 else
 fprintf('The Decoded Digits are :')
-% if Output == 0
-%     disp(char(10))
-%     fprintf('No Digits Detected')
-% end
-% if Output ~= 0
-%     Output
-% end
+
 Output
 end
 end
--- Final Project/MATLAB/GoertzelFilter.m
+++ Final Project/MATLAB/GoertzelFilter.m
@@ -1,5 +1,6 @@
 function [dftAllDigi] = GoertzelFilter ()
-
+%This function calls Goertzel1 and filters the signal N times, where N is the length of the startP array
+%Informally known as the amount of digits detected.
 close all;
 
 %%Inputs from other matlab files
@@ -7,16 +8,17 @@ Signal = fixed_encoder();
 [startP,endP] = DigitBreak();
 %%
 
-%%Setup variables
-Fs = 10e3;
-T = 1/Fs;
-f = [75 100 150 200 300 400 500];
-%%
+
 numDigit = length(startP); %checks how many digits for later computations.
+
+
 if numDigit==0
     fprintf('No tones Detected');
     dftAllDigi=0;
 end
+
+%This for loop takes 400 samples from the start of the signal and passes it through the Goertzel algorithm.
+%It's then transposed in accordance to how our Goertzel1 function operates
 if numDigit~=0
 for i=1:numDigit
     digiArray = Signal(startP(i):(startP(i)+400));
@@ -29,8 +31,9 @@ for i=1:numDigit
     end
 end
 
-
+%The Goertzel output is once more transposed for computations in later calculations.
 dftAllDigi=transpose(dftAllDigi);
+
 end
 end
 
